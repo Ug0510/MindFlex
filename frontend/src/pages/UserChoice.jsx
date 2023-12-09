6a3faba4ae7438d7beccfeaa0ac2680c42c8a350
Ug0510 <uditgg0510@gmail.com>
2023-12-10 01:25:34 +0530

    Added heading on UserChoice page

--- frontend/src/pages/UserChoice.jsx
+++ frontend/src/pages/UserChoice.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import '../assets/css/style.css';
 import '../assets/css/pages/UserChoice.css';
 import Background from '../assets/images/forest-background.png'
+import woodPlate from '../assets/images/wooden-plate.png'
 
 const UserChoice = () => {
   return (
     <>
     <body>
-    <img src={Background} className='w-100' alt="" />
+    <div className='bg-forest'>
+    <div className="heading-container">
+      <img src={woodPlate} alt="" className='heading-plate' />
+      <div class="wooden-text">
+        <div>Ultimate</div>
+       <div class="small">Genius</div>
+      </div>
+    </div>
+    </div>
     </body>
     </>
   );
