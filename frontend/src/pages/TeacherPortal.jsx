cd2c412118df7416099e59d421a287f525a17fb5
Ug0510 <uditgg0510@gmail.com>
2023-12-14 10:19:45 +0530

    Added Arrow button

--- frontend/src/pages/TeacherPortal.jsx
+++ frontend/src/pages/TeacherPortal.jsx
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 import '../assets/css/style.css';
 import woodPlate from '../assets/images/wooden-plate.png';
 
 const TeacherPortal = () => {
   const [gameCode, setGameCode] = useState('Game Code');
+  const [buttonStyle, setButtonStyle] = useState({ cursor: 'not-allowed', color: 'gray' });
 
   const generateRandomCode = () => {
     const randomCode = Math.floor(100000 + Math.random() * 900000); // Generates a random 6-digit code
     setGameCode(randomCode.toString());
+    setButtonStyle({ cursor: 'pointer', color: 'black' });
   };
 
   return (
     <div className="bg-forest">
-        <div className="heading-container">
-      <img src={woodPlate} alt="" className='heading-plate' />
-      <div class="wooden-text">
-        <div>Teacher</div>
-       <div class="small">Portal</div>
+      <div className="heading-container">
+        <img src={woodPlate} alt="" className='heading-plate' />
+        <div class="wooden-text">
+          <div>Teacher</div>
+          <div class="small">Portal</div>
+        </div>
       </div>
-    </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      
-      <input type="text" className='wood-border' value={gameCode} style={{maxWidth:'30vw'}} />
-      <input type="button" className="wood-button" value="Generate Code" onClick={generateRandomCode}/>
-      
-    </div>
+
+      {/* Code generation field and button  */}
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        <input type="text" className='wood-border' value={gameCode} style={{ maxWidth: '30vw', cursor: 'not-allowed' }} />
+        <input type="button" className="wood-button" value="Generate Code" onClick={generateRandomCode} />
+      </div>
+
+      <div className="arrow-btn">
+        <p className="arrow-text" >
+            <Link to="/StudentPortal" style={buttonStyle}>Start Quiz</Link>
+        </p>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/UserChoice.jsx
+++ frontend/src/pages/UserChoice.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/style.css';
 import '../assets/css/pages/UserChoice.css';
-import Background from '../assets/images/forest-background.png';
 import woodPlate from '../assets/images/wooden-plate.png';
 import choicePlate from '../assets/images/choice.png';
 
@@ -23,7 +22,6 @@ const UserChoice = () => {
     <div className="btn-container">
       <img src={choicePlate} alt="" />
       <p className='plate-text left'>
-        <a href=""></a>
         <Link to="/StudentPortal">Student</Link>
       </p>
       <img src={choicePlate} alt="" />
