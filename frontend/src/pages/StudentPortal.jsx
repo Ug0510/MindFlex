f52b1f8b733024258021cbafeda39a7454618640
Ug0510 <uditgg0510@gmail.com>
2023-12-15 08:14:24 +0530

    Added design of Student Portal

--- frontend/src/pages/StudentPortal.jsx
+++ frontend/src/pages/StudentPortal.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/css/style.css';
+import woodPlate from '../assets/images/wooden-plate.png';
 
-const StudentPortal = () =>
-{
-    return (
-        <>
-            <h1>Hi, student</h1>
-        </>
-    );
-}
+const StudentPortal = () => {
+  const [gameCode, setGameCode] = useState(''); // Store the entered code
+  const [buttonStyle, setButtonStyle] = useState({ cursor: 'not-allowed', color: 'gray' }); // Initial button state
+
+  // Update button style based on input length
+  const handleInputChange = (event) => {
+    setGameCode(event.target.value);
+    setButtonStyle({
+      cursor: event.target.value.length === 6 ? 'pointer' : 'not-allowed',
+      color: event.target.value.length === 6 ? 'black' : 'gray',
+    });
+  };
+
+  return (
+    <div className="bg-forest">
+      <div className="heading-container">
+        <img src={woodPlate} alt="" className="heading-plate" />
+        <div class="wooden-text">
+          <div>Student</div>
+          <div class="small">Portal</div>
+        </div>
+      </div>
+
+      {/* Code generation field and button  */}
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        <input
+          type="text"
+          className="wood-border"
+          style={{ maxWidth: '30vw' }}
+          placeholder="Enter Game Code"
+          value={gameCode}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="arrow-btn">
+        <p className="arrow-text">
+          <Link to={gameCode.length === 6 ? '/StudentPortal1' : ''} style={buttonStyle}>
+            Enter Quiz
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default StudentPortal;
