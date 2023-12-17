fafcde05d45012b3fff23e5e4a14f1939c51fe5b
Ug0510 <uditgg0510@gmail.com>
2023-12-17 14:28:11 +0530

    setting up new api on server side

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,59 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+const MONGO_URI = "mongodb://localhost/ultimate_genius"; // Get your MongoDB connection string from .env file
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Schema for storing game codes
+const GameCodeSchema = new mongoose.Schema({
+  code: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const GameCode = mongoose.model('GameCode', GameCodeSchema);
+
+// Enable CORS
+app.use(cors());
+
+// Parse incoming JSON data
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Quiz Code setup begin');
+  res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Define API route to receive game codes
+app.post('/api/game-codes', async (req, res) => {
+  console.log(req.body);
+  const { code } = req.body;
+  console.log(code);
+
+  // Validate the received code
+  if (!code || code.length !== 6) {
+    return res.status(400).send({ message: 'Invalid game code' });
+  }
+
+  // Create a new GameCode document
+  const newCode = new GameCode({ code });
+
+  try {
+    await newCode.save();
+    res.status(201).send({ message: 'Game code submitted successfully!' });
+  } catch (error) {
+    console.error('Error saving game code:', error);
+    res.status(500).send({ message: 'Internal server error' });
+  }
 });
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- frontend/src/pages/teacher/TeacherPortal.jsx
+++ frontend/src/pages/teacher/TeacherPortal.jsx
@@ -2,24 +2,37 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../assets/css/style.css';
 import woodPlate from '../../assets/images/wooden-plate.png';
+import axios from 'axios';
 
 const TeacherPortal = () => {
   const [gameCode, setGameCode] = useState('Game Code');
   const [buttonStyle, setButtonStyle] = useState({ cursor: 'not-allowed', color: 'gray' });
 
-  const generateRandomCode = () => {
-    const randomCode = Math.floor(100000 + Math.random() * 900000); // Generates a random 6-digit code
+  const generateRandomCode = async () => {
+    const randomCode = Math.floor(100000 + Math.random() * 900000);
     setGameCode(randomCode.toString());
     setButtonStyle({ cursor: 'pointer', color: 'black' });
+  
+    try {
+      const response = await axios.post('http://localhost:8000/api/game-codes', { code: randomCode });
+      console.log('here')
+      if (response.status === 201) {
+        console.log('Code submitted successfully!');
+      } else {
+        console.error('Error submitting code:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error submitting code:', error);
+    }
   };
 
   return (
     <div className="bg-forest">
       <div className="heading-container">
         <img src={woodPlate} alt="" className='heading-plate' />
-        <div class="wooden-text">
+        <div className="wooden-text">
           <div>Teacher</div>
-          <div class="small">Portal</div>
+          <div className="small">Portal</div>
         </div>
       </div>
 
