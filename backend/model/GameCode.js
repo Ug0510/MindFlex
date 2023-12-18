4ba982cee59394e6f8a544968f3d2ded8d551210
Ug0510 <uditgg0510@gmail.com>
2023-12-19 01:43:58 +0530

    Added automatic expiry time of 1 hour in gameCode

--- backend/model/GameCode.js
+++ backend/model/GameCode.js
@@ -4,6 +4,7 @@ const gameCodeSchema = new mongoose.Schema({
   code: { type: String, required: true, unique: true },
   teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher', required: true },
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
+  expiresAt: { type: Date, default: Date.now, expires: 60 * 60 }, // Expires in 1 hour
 });
 
 const GameCode = mongoose.model('GameCode', gameCodeSchema);
