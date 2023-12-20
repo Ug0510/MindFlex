042d9d23a935288cae7e7c7e20e152ac026ac23b
Ug0510 <uditgg0510@gmail.com>
2023-12-21 03:49:52 +0530

    Added UI in studentList page

--- frontend/src/pages/teacher/StudentList.jsx
+++ frontend/src/pages/teacher/StudentList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import woodPlate from '../../assets/images/wooden-plate.png';
 
 const StudentList = () => {
   const navigate = useNavigate();
@@ -8,6 +9,7 @@ const StudentList = () => {
   const [students, setStudents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const gameCode = localStorage.getItem('teachersGameCode');
+  const [buttonStyle, setButtonStyle] = useState({ cursor: 'pointer', color: 'black' });
   const token = localStorage.getItem('token');
 
   useEffect(() => {
@@ -61,25 +63,53 @@ const StudentList = () => {
   };
 
   return (
-    <div className="student-list-container">
-      <h3>Students Joining Quiz</h3>
-      {isLoading ? (
-        <p>Loading students...</p>
-      ) : (
-        students.length === 0 ? (
-          <p>No students have joined yet.</p>
-        ) : (
-          <ul>
-            {students.map((student) => (
-              <li key={student._id}>{student.fullName}</li>
-            ))}
-          </ul>
-        )
-      )}
-      <button onClick={startQuiz} disabled={students.length === 0}>
-        Start Quiz
-      </button>
+    <>
+    <div className="bg-forest">
+    <div className="heading-container">
+        <img src={woodPlate} alt="" className='heading-plate' />
+        <div className="wooden-text">
+          <div>Quiz Players</div>
+        </div>
     </div>
+      <div style={{position:'absolute',height:'70vh',bottom:'0',left:'0',width:'100vw',display:'flex',justifyContent:'center',alignItems:'end'}}>
+      <div className="both-side-pillar">
+        {students.length === 0 ? (
+             <>
+             <div className="pillar-board-container">
+              <div className="pillar-board" style={{width:'100%',marginTop:'15px'}}>
+                <p style={{paddingTop:'30px',marginLeft:'-20px'}}>Noone Join yet...</p>
+                <p style={{marginLeft:'-25px'}}>(Distribute quiz code)</p>
+              </div>
+             </div>
+             </>
+           ):(
+          <>
+            <div className="pillar-board-container">
+              <div className="pillar-board">
+                <p className='heading'>Students</p>
+              </div>
+              {students.map((student) => (
+                <div key={student._id} className='pillar-board'>
+                  <p>{student.fullName}</p>
+                  <p>{student.email}</p>
+                </div>
+              ))}
+            </div>
+              </>
+            )}
+             
+        </div>
+      </div>
+      
+
+      <div className="arrow-btn" style={{left:'auto',right:'10px'}}>
+        <p className="arrow-text">
+          <Link to={''} style={buttonStyle} onClick={startQuiz} disabled={students.length === 0}>Start Quiz</Link>
+        </p>
+      </div>
+    </div>
+
+    </>
   );
 };
 
