9fa8fb9011eff3f68dbcbd06082f06310d42f719
Ug0510 <uditgg0510@gmail.com>
2023-12-21 02:48:53 +0530

    Add UI in Quiz Dashboard (Result Screen)

--- frontend/src/pages/teacher/QuizDashboard.jsx
+++ frontend/src/pages/teacher/QuizDashboard.jsx
@@ -2,6 +2,9 @@
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../../assets/css/style.css';
+import woodPlate from '../../assets/images/wooden-plate.png';
+import pillar from '../../assets/images/pillar-big-space.png';
 
 const QuizDashboard = () => {
   const [gameCode, setGameCode] = useState(localStorage.getItem('game-code'));
@@ -32,9 +35,6 @@ const QuizDashboard = () => {
         }
       } catch (error) {
         console.error('Error fetching scores:', error);
-
-        // Set loading to false
-        setIsLoading(false);
       }
     };
 
@@ -48,22 +48,53 @@ const QuizDashboard = () => {
   }, [gameCode]);
 
   return (
-    <div>
-      <h1>Quiz Dashboard</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          <h2>Scores</h2>
-          <ul>
-            {scores.map((score) => (
-              <li key={score._id}>
-                Student Name: {score.fullName}, Email: {score.email}, Score: {score.score} points
-              </li>
-            ))}
-          </ul>
+    <div className='bg-forest'>
+       <div className="heading-container">
+        <img src={woodPlate} alt="" className='heading-plate' />
+        <div className="wooden-text">
+          <div>Quiz Result</div>
         </div>
-      )}
+      </div>
+      <div style={{position:'absolute',height:'70vh',bottom:'0',left:'0',width:'100vw',display:'flex',justifyContent:'center',alignItems:'end'}}>
+        
+        <div className="broad-pillar">
+        {isLoading ? (
+             <>
+             <div className="pillar-board-container">
+              <div className="pillar-board" style={{width:'100%'}}>
+                <p style={{paddingTop:'30px',marginLeft:'-20px'}}>Quiz is Live...</p>
+                <p style={{marginLeft:'-25px'}}>(Wait for Result)</p>
+              </div>
+             </div>
+             </>
+           ):(
+          <>
+            <div className="pillar-board-container">
+              <div className="pillar-board">
+                <p className='heading'>Student</p>
+              </div>
+              {scores.map((score) => (
+                <div key={score._id} className='pillar-board'>
+                  <p>{score.fullName}</p>
+                  <p>{score.email}</p>
+                </div>
+              ))}
+            </div>
+            <div className="pillar-board-container">
+                <div className="pillar-board">
+                  <p className='heading'>Score</p>
+                </div>
+                {scores.map((score) => (
+                  <div key={score._id} className='pillar-board'>
+                    <p className='score'>{score.score}</p>
+                  </div>
+                ))}
+            </div>
+          </>
+          )}
+             
+        </div>
+      </div>
     </div>
   );
 };
